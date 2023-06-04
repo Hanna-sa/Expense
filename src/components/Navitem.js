20701e17b1dc906a3eb06ba7640215c90e3a4147
Hanna Salam <134275521+Hanna-sa@users.noreply.github.com>
2023-06-05 00:13:32 +0530

    Initial commit

---
+++ src/components/Navitem.js
@@ -0,0 +1,14 @@
+export const Navitem = [
+  {
+    title: "PayTrack",
+    url: "/Dash",
+    cName: "nav-links",
+    icon: "fa-solid fa-right-left"
+  },
+  {
+    title: "Profile",
+    url: "/profile",
+    cName: "nav-links",
+    icon: "fa-solid fa-user"
+  }
+];
