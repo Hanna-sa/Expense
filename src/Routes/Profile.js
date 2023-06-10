d0eb29bfe29a6952902d1944c44e749657af182b
Hanna Salam <134275521+Hanna-sa@users.noreply.github.com>
2023-06-10 11:30:07 +0000

    just added required pages edit for further designing

---
+++ src/Routes/Profile.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+function Profile() {
+  return (
+    <>
+      <Navbar />
+      <h1 style={{ padding: 150 }}>This is Profile</h1>
+    </>
+  );
+}
+export default Profile;
---
+++ src/Routes/Signup.js
@@ -0,0 +1,10 @@
+
+function Signup() {
+  return (
+      <div>
+        <h1 style={{ padding: 150 }}>This is Signup</h1>
+      </div>
+    
+  );
+}
+export default Signup;
