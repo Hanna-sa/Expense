e4e72ab6d8fe1fda0e8565945fc6c0913e4c8be2
Hanna Salam <134275521+Hanna-sa@users.noreply.github.com>
2023-06-13 09:57:45 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { Component } from 'react';
 import "./Navstyle.css";
 import { Navitem } from "./Navitem";
 import { Link } from "react-router-dom";
