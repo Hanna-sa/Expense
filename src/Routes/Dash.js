2c6688caffad31c9a163b87d7a3c8d4d1caecabe
El-zabeth <134275751+El-zabeth@users.noreply.github.com>
2023-06-17 01:03:59 +0530

    Dash.js

--- src/Routes/Dash.js
+++ src/Routes/Dash.js
@@ -1,11 +1,49 @@
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import Navbar from "../components/Navbar";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import './Dash.css';
 
-function Dash() {
+const Dash = () => {
+  const [datas, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <>
+    <div className="dash-container">
       <Navbar />
-      <h1 style={{ padding: 150 }}>This is Dash</h1>
-    </>
+
+      <div className="table-container">
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell className="id-column">INCOME</TableCell>
+                <TableCell className="title-column">EXPENSE</TableCell>
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {datas.map((val, index) => (
+                <TableRow key={index}>
+                  <TableCell className="id-column">{val.id}</TableCell>
+                  <TableCell className="title-column">{val.title}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
   );
-}
+};
+
 export default Dash;
